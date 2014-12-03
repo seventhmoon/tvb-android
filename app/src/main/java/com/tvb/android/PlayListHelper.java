b08b9fff13270922ecee1cd9db64504d76e5cb1c
seventhmoon <funglam@7thmoon.com>
2014-12-03 23:34:04 +0800

    Created Helper for getting HLS playlist

---
+++ app/src/main/java/com/tvb/android/PlayListHelper.java
@@ -0,0 +1,49 @@
+package com.tvb.android;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Created by fung on 03/12/2014.
+ */
+public class PlayListHelper {
+
+    public enum Channel {HDJ, J2, INEWS};
+    private static String[] links = {"http://token.tvb.com/stream/live/hls/mobilehd_hdj.smil",
+            "http://token.tvb.com/stream/live/hls/mobilehd_j2.smil",
+            "http://token.tvb.com/stream/live/hls/mobilehd_inews.smil"};
+
+
+    public static String getPlaylist(Channel channel) {
+        try {
+            String link = null;
+            switch (channel) {
+                case HDJ:
+                    link = links[0];
+                    break;
+                case J2:
+                    link = links[1];
+                    break;
+                case INEWS:
+                    link = links[2];
+                    break;
+            }
+            URL url = new URL(link);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setInstanceFollowRedirects(false);
+            int code = conn.getResponseCode();
+            Map map = conn.getHeaderFields();
+            if (code == 302) {
+                return map.get("Location").toString().replaceAll("\\[", "").replaceAll("\\]", "");
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
